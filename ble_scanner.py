ba03b66b674b748e681b68113a88e473ce16f2c3
Philemon Schweizer <philemon.schweizer@gmail.com>
2022-07-12 07:52:35 +0200

    info message format changed

--- ble_scanner.py
+++ ble_scanner.py
@@ -49,6 +49,13 @@ fh.setLevel(logging.INFO)
 # create console handler with a higher log level
 ch = logging.StreamHandler()
 ch.setLevel(logging.ERROR)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+# add the handlers to the logger
+log.addHandler(fh)
+log.addHandler(ch)
 
 # parse arguments
 parser = argparse.ArgumentParser(description = 'Victron modbus control test')
